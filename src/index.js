980cb4a90ace02f5d6fe807ea0165ed91de781bd
maperezotero <maperezotero@gmail.com>
2021-07-13 21:07:04 +0200

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+import './styles.css';
+
+export const todoList = new TodoList();
+todoList.todos.forEach( todo => crearTodoHtml( todo ) );
+
